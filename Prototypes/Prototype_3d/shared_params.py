73bb9a9038607c1fc6b536ee45bfcb1544543ef2
Kliooo <slavabek.games@gmail.com>
2025-04-23 21:17:37 +0300

    [VBek] Update_sketch_of_the_final_program_in_3d

--- Prototypes/Prototype_3d/shared_params.py
+++ Prototypes/Prototype_3d/shared_params.py
@@ -3,17 +3,20 @@ import os
 
 default_settings = {
     "Скорость тела (v), м/с:": 7000,
-    "Плотность мишени (ρ_t), кг/м³:": 2500,
+    "Плотность мишени (ρₜ), кг/м³:": 2500,
     "Прочность мишени (τ₀), Па:": 5000000,
-    "Дин. прочность (Y), Па:": 45000000,
-    "Стат. прочность (σ_t), Па:": 50000000,
     "Скорость звука (c), м/с:": 4000,
-    "Ускорение свободного падения (g), м/с²:": 9.81,
     "Диаметр тела (L), м:": 0.29,
-    "Плотность тела (ρ_p), кг/м³:": 8500,
+    "Плотность тела (ρₚ), кг/м³:": 8500,
     "Масса тела (m), кг:": 0.52 * 8500 * 0.29**3,
-    "Диаметр кратера (Dt_2_2), м:": 4.712683,
-    "Глубина кратера (ht_2_2), м:": 0.890781
+    "Коэффициент проницаемости (kₚᵣ):": 0.0000007,
+    "Коэффициент потери массы (α):": 0.40,
+    "Критическая скорость (Vₖᵣ), м/с:": 2000,
+    "Длина цилиндра (N), м:": 0.19,
+    "Диаметр кратера (Dₜ), м:": 4.712683,
+    "Глубина кратера (hₜ), м:": 0.890781,
+    "Глубина проникновения (hₛ), м:": 1.098692,
+    "Радиус поражённой зоны (rₛ), м:": 0.177588
 }
 
 SETTINGS_FILE = "settings.json"
@@ -27,8 +30,11 @@ def load_settings():
             with open(SETTINGS_FILE, 'r', encoding='utf-8') as f:
                 loaded_settings = json.load(f)
                 shared_settings.update({k: v for k, v in loaded_settings.items() if k in default_settings})
+                for key in default_settings:
+                    if key not in shared_settings:
+                        shared_settings[key] = default_settings[key]
         except (json.JSONDecodeError, IOError):
-            pass
+            shared_settings = default_settings.copy()
 
 def save_settings():
     try:
